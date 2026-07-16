6ba0e062c29385c5b07ea489cd39160eda4b15e0
Sandi Miftah <sandiseeyou@gmail.com>
2026-07-16 17:19:32 +0700

    feature: update a little

---
+++ src/components/navigations/ToggleDarkMode.tsx
@@ -0,0 +1,19 @@
+
+import { PiSunDuotone, PiMoonDuotone } from "react-icons/pi";
+
+export default function ToggleDarkMode() {
+  return (
+    <label
+      className="swap swap-rotate rounded-md p-2 text-primary transition-colors duration-200 hover:bg-secondary/10"
+      aria-label="Toggle dark mode"
+    >
+      <input
+        type="checkbox"
+        value="desindes-dark"
+        className="theme-controller"
+      />
+      <PiSunDuotone className="swap-off h-5 w-5" />
+      <PiMoonDuotone className="swap-on h-5 w-5" />
+    </label>
+  );
+}
